07c9b03b61c9f9d1d267835ddeb410175693ebc1
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-08-30 14:42:58 -0300

    Programa em Java que exibe números de 100 a 1 utilizando estrutura de repetição

---
+++ MostraNumeros100a1.java
@@ -0,0 +1,10 @@
+public class MostraNumeros100a1 {
+    public static void main(String[] args) {
+        System.out.println("Este programa mostra os número de 100 a 1");
+        int i = 100;
+        while (i <= 100 & i >=1){
+            System.out.println(i);
+            i--;
+        }
+    }
+}
