dfe7911f3f0e99fec8ad7ac4c70872e740d8da18
Márcio de Freitas Viana <marcio.viana@zup.com.br>
2021-08-30 14:32:17 -0300

    Programa em Java que usa estrutura de repetição para imprimir de 1 a 100

---
+++ MostraNumeros1a100.java
@@ -0,0 +1,10 @@
+public class MostraNumeros1a100 {
+    public static void main(String[] args) {
+        System.out.println("Este programa mostra na tela os números de 1 a 100");
+        int i = 1;
+        while (i <= 100){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
